245748e1b5338b5647e355217d6a7ce8ce48624d
Administrator <Administrator@192.168.12.101>
2018-12-05 11:37:19 +0100

    LocalDate example

--- oved185/src/eg/Hello.java
+++ oved185/src/eg/Hello.java
@@ -1,7 +1,10 @@
 package eg;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello @" + LocalDate.of(2018, Month.DECEMBER, 5));
 	}
 }
